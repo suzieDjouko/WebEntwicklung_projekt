9a10c5f7544d7c9deea792c8dca1e6c0284c912c
suzieDjouko <djoukomarcelle@gmail.com>
2025-01-15 14:37:56 +0100

    add link main.js

--- styles/main.js
+++ styles/main.js
@@ -7,8 +7,8 @@ footer.innerHTML =
       <div>CONTACT US</div>
       <div>
         <a href=""><span>WhatsApp</span><i class="fa-brands fa-whatsapp"></i></a>
-        <a href=""><span>Email</span><i class="fa-regular fa-envelope"></i></a>
-        <a href=""><span>Phone</span><i class="fa-solid fa-phone"></i></a>
+        <a href="mailto:inf4302@hs-worms.de"><span>Email</span><i class="fa-regular fa-envelope"></i></a>
+        <a href="tel:017687790133"><span>Phone</span><i class="fa-solid fa-phone"></i></a>
       </div>
       
       </div>
@@ -16,19 +16,19 @@ footer.innerHTML =
            <div>FOLGE UNS</div>
           <div>
           
-            <a href="#">
+            <a href="https://www.facebook.com/mintzukunftschaffen/?locale=de_DE" target="_blank" >
               <i class="fa-brands fa-facebook"></i>
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/mint_zirkel/" target="_blank">
               <i class="fa-brands fa-instagram"></i>
             </a>
             <a href="#">
               <i class="fa-brands fa-x-twitter"></i>
             </a>
-            <a href="#">
+            <a href="https://www.worms.de/neu-de/bildung-bieten/bildungsbuero/MINT-Angebote/" target="_blank">
               <i class="fa-brands fa-google"></i>
             </a>
-            <a href="#">
+            <a href="https://www.youtube.com/watch?v=ZlvSJ6l1Q8g" target ="_blank">
               <i class="fa-brands fa-youtube"></i>
             </a>
           </div>
